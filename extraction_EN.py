7ccc84352687f275b4dad4cc5a3012ebda6317b5
paoHerreno <pao.13.her@gmail.com>
2021-02-23 10:20:26 +0100

    modification EN

--- extraction_EN.py
+++ extraction_EN.py
@@ -9,8 +9,8 @@ dic_entites = {}
 
 for k, v in titres.items():
     doc = nlp(v)
-    list_ent = ["_"]
-    list_etiq = ["_"]
+    list_ent = []
+    list_etiq = []
     for ent in doc.ents: 
         list_ent.append(ent.text)
         list_etiq.append(ent.label_)
